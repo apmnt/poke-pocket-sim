44c7d89251057fe9236c56892d18f496bacad2ca
apmnt <aapo.montin@student.oulu.fi>
2024-11-23 03:22:12 +0200

    Remove redundant things and add card example

--- card.py
+++ card.py
@@ -1,5 +1,19 @@
 import uuid
+from enum import Enum
 from action import Action, ActionType
+from attack import Attack
+
+
+class CardsEnum(Enum):
+    MEWTWO_EX = "Mewtwo EX"
+
+
+CARDS_DICT = {
+    CardsEnum.MEWTWO_EX: {
+        "hp": 150,
+        "attacks": [Attack.psydrive, Attack.psychic_sphere],
+    }
+}
 
 
 class Card:
@@ -8,12 +22,9 @@ class Card:
         name,
         hp,
         type=None,
-        energies=None,
         attacks=None,
         retreat_cost=0,
-        modifiers=None,
         ability=None,
-        conditions=None,
         weakness=None,
         is_basic=True,
     ):
@@ -21,12 +32,12 @@ class Card:
         self.name = name
         self.hp = hp
         self.type = type
-        self.energies = energies if energies is not None else {}
+        self.energies = {}
         self.attacks = attacks
         self.retreat_cost = retreat_cost
-        self.modifiers = modifiers
+        self.modifiers = []
         self.ability = ability
-        self.conditions = conditions
+        self.conditions = []
         self.weakness = weakness
         self.is_basic = is_basic
 
@@ -46,6 +57,13 @@ class Card:
         else:
             self.energies[energy] = 1
 
+    def remove_energy(self, energy):
+        if energy not in self.energies:
+            raise ValueError(f"Energy type {energy} not found in energies.")
+        if self.energies[energy] <= 0:
+            raise ValueError(f"Energy count for {energy} is already 0 or less.")
+        self.energies[energy] -= 1
+
     def get_total_energy(self):
         return sum(self.energies.values())
 
@@ -54,3 +72,7 @@ class Card:
             f"{energy}: {amount}" for energy, amount in self.energies.items()
         )
         return f"Card({self.name} with {self.hp} hp, Energies: {energies_str})"
+
+    @staticmethod
+    def create_card(card: CardsEnum):
+        return Card(card)
