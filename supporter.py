7abae307227292c2a1e2b935d5f24167d8ab9258
apmnt <aapo.montin@student.oulu.fi>
2024-11-24 13:18:33 +0200

    Add Sabrina and Giovanni

--- supporter.py
+++ supporter.py
@@ -1,5 +1,6 @@
 from enum import Enum
 import random
+from condition import Condition
 
 
 class Supporter:
@@ -13,4 +14,13 @@ class Supporter:
 
     class Giovanni:
         def use(player):
-            pass
+            player.active_card.add_condition(Condition.Plus10DamageDealed)
+            for card in player.bench:
+                card.add_condition(Condition.Plus10DamageDealed)
+
+    class Sabrina:
+        def player_able_to_use(player):
+            return player.opponent.active_card and len(player.opponent.bench) > 0
+
+        def use(player):
+            player.opponent.move_active_card_to_bench()
