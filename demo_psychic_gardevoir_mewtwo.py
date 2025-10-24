199e93a9e179a82799e1f0c7cd9db61b4fa87dee
KFSPC8\Arne <arne.95@hotmail.com>
2025-10-25 02:21:27 +0800

    Update demo file

--- demo_psychic_gardevoir_mewtwo.py
+++ demo_psychic_gardevoir_mewtwo.py
@@ -1,5 +1,5 @@
 import builtins
-from pokepocketsim import Player, Deck, Card, Item, Match, Cards
+from pokepocketsim import Player, Deck, Card, Item, Match
 
 # Global list to record inputs
 recorded_inputs = []
@@ -19,17 +19,17 @@ builtins.input = recording_input
 
 # Setup decks, players and match
 test_deck1 = Deck(energy_types=["psychic"])
-test_deck1.add_card(Card.create_card(Cards.RALTS))
-test_deck1.add_card(Card.create_card(Cards.KIRLIA))
-test_deck1.add_card(Card.create_card(Cards.GARDEVOIR))
-test_deck1.add_card(Card.create_card(Cards.MEWTWO_EX))
+test_deck1.add_card(Card.create_card("Ralts"))
+test_deck1.add_card(Card.create_card("Kirlia"))
+test_deck1.add_card(Card.create_card("Gardevoir"))
+test_deck1.add_card(Card.create_card("Mewtwo EX"))
 test_deck1.add_card(Item.Potion)
 test_deck1.add_card(Item.Potion)
 
 test_deck2 = Deck(energy_types=["psychic"])
-test_deck2.add_card(Card.create_card(Cards.RALTS))
-test_deck2.add_card(Card.create_card(Cards.RALTS))
-test_deck2.add_card(Card.create_card(Cards.RALTS))
+test_deck2.add_card(Card.create_card("Ralts"))
+test_deck2.add_card(Card.create_card("Ralts"))
+test_deck2.add_card(Card.create_card("Ralts"))
 
 test_player1 = Player("p1", test_deck1, is_bot=False)
 test_player2 = Player("p2", test_deck2, is_bot=False)
